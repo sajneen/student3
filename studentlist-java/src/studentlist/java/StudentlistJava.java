ede26033c39f768c49faf7faa82574e4e3179eb2
shadia1837820110 <shadiasajneen@gmail.com>
2019-07-14 11:48:36 +0600

    project initialized

---
+++ studentlist-java/src/studentlist/java/StudentlistJava.java
@@ -0,0 +1,21 @@
+i/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlist.java;
+
+/**
+ *
+ * @author user
+ */
+public class StudentlistJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
